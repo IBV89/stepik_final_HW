ed40c02120231fba23506c0cc4410cd478a0937b
Ilya B <ilbv@mail.ru>
2020-12-30 15:42:51 +0300

    added tests for cart

---
+++ pages/cart_page.py
@@ -0,0 +1,11 @@
+from .base_page import BasePage
+from .locators import CartPageLocators
+
+class CartPage(BasePage):
+    def should_cart_empty(self):
+        assert self.is_not_element_present(*CartPageLocators.CART_FORM), \
+            'Form in cart must be not showing, but it shows'
+
+    def should_cart_is_empty_text(self):
+        assert self.is_element_present(*CartPageLocators.CART_IS_EMPTY_TEXT), \
+            'Text for empty basket is not present'
